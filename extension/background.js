0025fd8fabc8a8a419d6112d06a664f6f6f9b60f
cjannun <93235878+cjannun@users.noreply.github.com>
2025-01-25 16:17:37 -0500

    Fixed query tracking in background.js

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,7 @@
 let queryCount = 0;
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "incrementQueryCount") {
-    console.log("Incremented query count");
-    // Get current queryCount from storage, then increment
-    chrome.storage.session.get("queryCount", (data) => {
-      let queryCount = data.queryCount || 0;
-      queryCount++;
-      chrome.storage.session.set({ queryCount }, () => {
-        // Send the updated queryCount back as a response
-        sendResponse({ queryCount });
-      });
-    });
-    return true;  // Indicate that the response will be sent asynchronously
-  } else if (message.action === "getQueryCount") {
+  if (message.action === "getQueryCount") {
     console.log("Getting query count...");
     chrome.storage.session.get("queryCount", (data) => {
       const queryCount = data.queryCount || 0;
@@ -24,13 +12,21 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
+function incrementQueryCount() {
+  chrome.storage.session.get("queryCount", (data) => {
+      let queryCount = data.queryCount || 0;
+      queryCount++;
+      chrome.storage.session.set({ queryCount }, () => {
+          console.log("Query count updated to:", queryCount);
+      });
+  });
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     console.log("Request to OpenAI API detected:", details.url);
-    chrome.runtime.sendMessage({ action: "incrementQueryCount" }, (response) => {
-      // Handle response from listener
-      console.log("Response from increment:", response);
-    });
+    incrementQueryCount();
+    console.log("Sent request to increment");
   },
   { urls: ["https://chatgpt.com/backend-api/lat/r"] },
   ["requestBody"]
