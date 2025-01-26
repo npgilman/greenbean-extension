18bfc3c68ac93685a5e69f01b1de4448bd88fa46
Nathan Gilman <ngilman@ufl.edu>
2025-01-25 23:16:16 -0500

    Add firebase connectoin with auth0

--- greenbean/js/firebase/firebaseUtils.js
+++ greenbean/js/firebase/firebaseUtils.js
@@ -1,13 +1,22 @@
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, query, where, doc, setDoc, documentId } from "firebase/firestore";
 import { db } from './firebaseConfig.js';
 
-export async function getDataFromFirestore() {
+
+export async function getUserData(userId) {
   try {
-    const querySnapshot = await getDocs(collection(db, 'users'));
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
+    const q = query(collection(db, 'users'), where(documentId(), "==", userId));
+    return await getDocs(q);
   } catch (e) {
     console.error("Error getting documents: ", e);
   }
+}
+
+
+export async function enrollUser(userId, name) {
+  await setDoc(doc(db, "users", userId), {
+    userName: name,
+    dateJoined: new Date(),
+    queryCounter: 0,
+    queryTimes: []
+  });
 }
